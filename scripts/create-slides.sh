e25aa0ae4d562d61383c96c7034919557b8f1a23
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-10-28 12:14:46 +0200

    [wip] try to find markdown files automatically

---
+++ scripts/create-slides.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+
+for f in $(find . -iname "*_slides.md");
+do
+    echo $f
+    output_filename=$(echo $f | rev| cut -d "/" -f 1 | rev | sed "s/md/pdf/")
+    echo $output_filename
+    pandoc -t beamer -V aspectratio=169 -V linkcolor:blue -V fontsize=12pt --listings -s --output=slides/${output_filename} ${f}
+done

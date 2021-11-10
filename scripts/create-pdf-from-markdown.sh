f16ed6c4c73bcda8d19f60bf7e437babbe0d7ec2
Alexander Jaust <alexander.jaust@ipvs.uni-stuttgart.de>
2021-11-10 17:37:42 +0100

    [fix][ci] Make pandoc build scripts recognize all pandoc options
    The pandoc options are stored in a bash array. In the previous version of the scripts did **not** consider all array elements

--- scripts/create-pdf-from-markdown.sh
+++ scripts/create-pdf-from-markdown.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PANDOC_SLIDE_OPTIONS=("-t beamer" "-V aspectratio=169" "-V linkcolor:blue" "-V fontsize=12pt" "--listings" "-s")
-PANDOC_TEXT_OPTIONS=("-V geometry:a4paper" "-V geometry:left=2.5cm" "-V geometry:right=2.5cm" "-V geometry:bottom=2.5cm" "-V geometry:top=2.5cm" "-V linkcolor:blue" "-V fontsize=10pt" "--listings" "-s")
+PANDOC_TEXT_OPTIONS=("-V geometry:a4paper" "-V geometry:left=2.5cm" "-V geometry:right=2.5cm" "-V geometry:bottom=2.5cm" "-V geometry:top=2.5cm" "-V colorlinks:true" "-V linkcolor:blue" "-V fontsize=10pt" "--listings" "-s")
 
 if [[ $# == 1 ]];
 then
@@ -24,10 +24,10 @@ fi
 
 if [[ "$TARGET_FILES" == "slides" ]];
 then
-    output_options="${PANDOC_SLIDE_OPTIONS}"
+    output_options="${PANDOC_SLIDE_OPTIONS[@]}"
     target_base="slides"
 else
-    output_options="${PANDOC_TEXT_OPTIONS}"
+    output_options="${PANDOC_TEXT_OPTIONS[@]}"
     target_base="texts"
 fi
 
